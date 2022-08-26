aaf53a34c789c03e2807835ae2ef12d7c1746db4
vigneshrml <vickysathy22@gmail.com>
2022-08-26 15:15:04 +0530

    error solve

--- pos-frontend/tailwind.config.js
+++ pos-frontend/tailwind.config.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-=======
 /** @type {import('tailwindcss').Config} */ 
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
->>>>>>> 6e727db789929ff10d8cee79fcdf6d818ff1737f
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 6e727db789929ff10d8cee79fcdf6d818ff1737f
