7031f6d979f9ef9ba4dac5303b89fff83d4f2b78
vigneshrml <vickysathy22@gmail.com>
2022-08-26 14:43:09 +0530

    Router Config

---
+++ pos-frontend/src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+import product from "@/store/modules/product.store"
+
+Vue.use(Vuex);
+
+
+export default new Vuex.Store({
+    modules : {
+      product
+    }
+  });
+  
