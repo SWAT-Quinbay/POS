a5123915dfce52881112a52b5048dccf35ed0b2a
vigneshrml <vickysathy22@gmail.com>
2022-08-26 12:43:39 +0530

    added utils/demoproducts

---
+++ pos-frontend/src/utils/demoProducts.js
@@ -0,0 +1,104 @@
+const products = [
+    {
+      "name": "Generic Metal Towels",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "976.00",
+      "id": "1"
+    },
+    {
+      "name": "Recycled Steel Shirt",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "998.00",
+      "id": "2"
+    },
+    {
+      "name": "Licensed Fresh Mouse",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "759.00",
+      "id": "3"
+    },
+    {
+      "name": "Elegant Granite Hat",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "771.00",
+      "id": "4"
+    },
+    {
+      "name": "Bespoke Frozen Shirt",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "478.00",
+      "id": "5"
+    },
+    {
+      "name": "Electronic Plastic Chair",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "989.00",
+      "id": "6"
+    },
+    {
+      "name": "Handmade Granite Car",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "268.00",
+      "id": "7"
+    },
+    {
+      "name": "Incredible Metal Soap",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "517.00",
+      "id": "8"
+    },
+    {
+      "name": "Luxurious Plastic Shoes",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "538.00",
+      "id": "9"
+    },
+    {
+      "name": "Licensed Concrete Hat",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "104.00",
+      "id": "10"
+    },
+    {
+      "name": "Refined Granite Bike",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "321.00",
+      "id": "11"
+    },
+    {
+      "name": "Awesome Bronze Keyboard",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "403.00",
+      "id": "12"
+    },
+    {
+      "name": "Tasty Frozen Tuna",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "611.00",
+      "id": "13"
+    },
+    {
+      "name": "Fantastic Plastic Shoes",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "30.00",
+      "id": "14"
+    },
+    {
+      "name": "Licensed Wooden Chips",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "113.00",
+      "id": "15"
+    },
+    {
+      "name": "Awesome Cotton Tuna",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "537.00",
+      "id": "16"
+    },
+    {
+      "name": "Tasty Soft Cheese",
+      "image": "http://loremflickr.com/640/480/food",
+      "price": "673.00",
+      "id": "17"
+    }
+  ]
